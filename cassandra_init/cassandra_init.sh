486a3f1f04e9746a225c646ee9481cc53bfe163b
vdimitriev <vdimitriev@protonmail.com>
2022-03-02 19:08:49 +0100

    Update cassandra init sh file.

--- cassandra_init/cassandra_init.sh
+++ cassandra_init/cassandra_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-CQL="CREATE KEYSPACE IF NOT EXISTS slavim_chat WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
+CQL="CREATE KEYSPACE IF NOT EXISTS slavim_chat WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+CREATE TABLE IF NOT EXISTS slavim_chat.messages (username text, chatRoomId text, date timestamp, fromUser text, toUser text, text text, PRIMARY KEY ((username, chatRoomId), date));"
 
 until echo $CQL | cqlsh; do
   echo "cqlsh: Cassandra is unavailable to initialize - will retry later"
