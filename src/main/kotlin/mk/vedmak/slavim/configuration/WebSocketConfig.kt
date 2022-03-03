7b7f8dacb3ed5054a77aa63d2e83122c994166bb
vdimitriev <vdimitriev@protonmail.com>
2022-03-03 08:57:41 +0100

    Update stomp broker relay in web socket config.

--- src/main/kotlin/mk/vedmak/slavim/configuration/WebSocketConfig.kt
+++ src/main/kotlin/mk/vedmak/slavim/configuration/WebSocketConfig.kt
@@ -1,5 +1,6 @@
 package mk.vedmak.slavim.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.session.ExpiringSession
@@ -11,13 +12,20 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 @EnableWebSocketMessageBroker
 class WebSocketConfig : AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession>() {
 
+    private var relayHost: String = "localhost"
+    private var relayPort: Int = 61613
 
     override fun configureStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws").withSockJS()
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-        registry.enableSimpleBroker("/topic", "/queue")
+        registry.enableStompBrokerRelay("/queue/", "/topic/")
+            .setUserDestinationBroadcast("/topic/unresolved.user.dest")
+            .setUserRegistryBroadcast("/topic/registry.broadcast")
+            .setRelayHost(relayHost)
+            .setRelayPort(relayPort)
+
         registry.setApplicationDestinationPrefixes("/chatroom")
     }
 }
